535cc583e078ecda2a9e92d16c4a6557ac407a2e
James Hudson <jmh357@comcast.net>
2022-11-01 11:56:04 -0500

    solve challenge 4

--- python-functions.py
+++ python-functions.py
@@ -26,4 +26,14 @@ def largest(list):
 def occurrences(str1, str2):
   return str1.count(str2)
 
-print(occurrences('fleep floop', 'e'))
+# print(occurrences('fleep floop', 'e'))
+
+# ----------------------------------- CHALLENGE 4 -----------------------------------
+
+def product(*args):
+  product = 1
+  for arg in args:
+    product = arg*product
+  return product
+
+# print(product(4, 0.5, 5))
